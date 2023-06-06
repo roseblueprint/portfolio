aca76c72fbc6c13cab9ec9af842a3a5179f5519d
Roseblueprint <roxanephilippine@gmail.com>
2023-06-06 21:12:19 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@
 import Header from "./component/header/Header";
 import About from "./component/about/About";
 import Navbar from "./component/navbar/navbar";
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 
 
@@ -12,7 +18,12 @@ function App(){
         <Header />
         <Navbar />
         <About />
-        
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
 
         </>
     );
